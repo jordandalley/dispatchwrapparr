adbf0d306a705a0f74cf1a99c21968a090b28713
3l3m3nt <jordan@dalleyfamily.net>
2026-05-11 15:56:43 +1200

    Initial commit for upcoming release Changed: - Major refactor since the implementation of 'stream-passthrough-encrypted' Streamlink option implemented in 8.4.0 - Removed excess DRM related monkey patch classes - Reduced stream detection behaviour - Introduced SingleMuxedStream class to force muxing of single streams for DRM decryption - Added additional monkey patches thanks to Streamlink's rigid dash implementation
    Bugfixes:
    - Ffmpeg file checks in argparse
    - Pass ffmpeg binary location to PlayRadio class
    
    Deprecations:
    - Removal of -subitles option. Won't implement.

--- plugin.py
+++ plugin.py
@@ -10,7 +10,7 @@ from apps.plugins.models import PluginConfig
 
 class Plugin:
     name = "Dispatchwrapparr"
-    version = "1.6.2"
+    version = "1.7.0"
     description = "An intelligent DRM/Clearkey capable stream profile for Dispatcharr"
     profile_name = "Dispatchwrapparr"
     install_path = "/data/dispatchwrapparr/dispatchwrapparr.py"
